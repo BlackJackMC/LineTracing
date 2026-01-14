c1488c0006e6b86588bd84693a10271117b5aa08
FubuGold <trangiahuy211207@gmail.com>
2026-01-14 20:37:50 +0700

    feat: tested with 1 light

--- TrafficLight/src/main.cpp
+++ TrafficLight/src/main.cpp
@@ -8,17 +8,11 @@ void setup()
     Serial.begin(9600);
     while (!Serial)
         ;
-        // startTL();
-    pinMode((int)Pin_South::RED_PIN,OUTPUT);
-    pinMode((int)Pin_South::YELLOW_PIN,OUTPUT);
-    pinMode((int)Pin_South::GREEN_PIN,OUTPUT);
+    startTL();
 }
     
 void loop()
 {
-    digitalWrite((int)Pin_South::RED_PIN,HIGH);
-    digitalWrite((int)Pin_South::YELLOW_PIN,HIGH);
-    digitalWrite((int)Pin_South::GREEN_PIN,HIGH);
     Serial.println("Running");
     delay(1000);
 }
